0cc4c7c6ed1d32d02ce1611a2bc9edf5fd60efde
lgq186 <1043736808@qq.com>
2019-02-19 16:37:22 +0800

    vue.js试听课程项目代码

---
+++ my-first-vue-project/src/components/componentA.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1>{{ msgfromfather }}</h1>
+    <button v-on:click="onClickMe">open mouse!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      msg: 'hello from component a!'
+    }
+  },
+  props: ['msgfromfather'],
+  methods: {
+    onClickMe: function () {
+      // console.log(this.msgfromfather)
+      this.$emit('child-tell-me-something', this.msg);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
